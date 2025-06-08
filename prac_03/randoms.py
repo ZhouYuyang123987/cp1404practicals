53c6cebee4c1039edc14a91e390c971d7f520422
zhouyuyang <yuyang.zhou@my.jcu.edu.au>
2025-06-08 12:24:06 +0800

    add randoms.py

---
+++ prac_03/randoms.py
@@ -0,0 +1,6 @@
+import random
+
+print(random.randint(5, 20))  # line 1 smallest:5   largest:18
+print(random.randrange(3, 10, 2))  # line 2  smallest:4 largest:7 ,no 4
+print(random.uniform(2.5, 5.5))  # line 3 smallest:2.75 largest:5.4
+print(random.randint(1,100))
